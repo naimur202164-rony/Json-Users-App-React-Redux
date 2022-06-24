0c31a4ffcba6816ddb13732bfd1e77d53494fe9a
naimur <naimur202163@gmail.com>
2022-06-24 11:02:35 +0600

    done

--- backend/app.js
+++ backend/app.js
@@ -9,62 +9,66 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json());
 
+
+
 app.get("/users", async (req, res) => {
   try {
     const userss = await pool.query("SELECT * FROM users");
-    const users=userss.rows[0].users
+    const users = userss.rows;
     res.send(users);
   } catch (error) {
     console.log(error);
   }
 });
 
-// app.get("/users/:id", async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     const user = await pool.query("SELECT * FROM users WHERE id=$1", [id]);
-//     res.send(user.rows[0]);
-//   } catch (err) {
-//     console.log(err);
-//   }
-// });
+
+
 app.get("/users/:id", async (req, res) => {
   try {
     const { id } = req.params;
     const user = await pool.query("SELECT * FROM users WHERE id=$1", [id]);
-    const users=
     res.send(user.rows[0].users);
   } catch (err) {
     console.log(err);
   }
 });
 
+
+
+
+
 // Get all posts
 app.get("/posts", async (req, res) => {
   try {
     const userPost = await pool.query("SELECT * FROM posts");
-    res.send(userPost.rows[0].posts);
-  } catch (err) {
-    console.log(err);
-  }
-});
-
-// Get Single post
-
-app.get("/posts/:id", async (req, res) => {
-  try {
-    
-    const { id } = req.params;
-    const  quer = req.query;
-    console.log(quer);
-    const userPost = await pool.query("SELECT * FROM posts WHERE id=$1", [id]);
     res.send(userPost.rows);
   } catch (err) {
     console.log(err);
   }
 });
 
+
+
+
+// Get Single post
+
+app.get("/posts/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const quer = req.query;
+    console.log(quer);
+    const userPost = await pool.query("SELECT * FROM posts WHERE id =$1", [id]);
+    const postId = userPost.rows.posts;
+    res.send(postId);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
 // Get All comments
+
+
+
 
 app.get("/comments", async (req, res) => {
   try {
@@ -74,6 +78,12 @@ app.get("/comments", async (req, res) => {
     console.log(err);
   }
 });
+
+
+
+
+
+
 
 app.listen(port, () => {
   console.log(`the server is running on port ${port}`);
